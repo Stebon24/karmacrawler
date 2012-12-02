621a38d3e4ec0047d0a083dec9feaf8bc4a7020b
stebon24 <stebon24@gmail.com>
2012-12-01 18:56:55 -0800

    updated app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , mine = require('./routes/mine')
   , http = require('http')
   , path = require('path');
 
@@ -29,6 +30,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/user/:username', mine.miner);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
