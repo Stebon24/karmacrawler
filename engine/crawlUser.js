32724c034084cc30b62225af8a22d0edbace1919
stebon24 <stebon24@gmail.com>
2012-12-02 21:17:37 -0800

    now captures and parses JSON from reddit

--- engine/crawlUser.js
+++ engine/crawlUser.js
@@ -1,18 +1,30 @@
 var http = require('http');
-var l = console.log;
 var options = {
 	host: 'www.reddit.com',
 	path: ''
 };
 
-module.exports = function (username) {
-		l(username);
-		options.path = '/user/' + username + '.json';
-		http.get(options, function(res) {
-			res.setEncoding('utf8');
-			res.on('data', function(data){
-				var userData = JSON.parse(data);
-				console.log(userData);
-			});
-		}).end();
-	};
+module.exports = function(username) {
+
+	console.log(username);
+
+	options.path = '/user/' + username + '.json';
+
+	var userData = '';
+	
+	http.get(options, function(res) {
+
+		res.setEncoding('utf8');
+
+		res.on('data', function(data) {
+			userData += data;
+			console.log(userData);
+		});
+
+		res.on('end', function() {
+			userData = JSON.parse(userData);
+			console.log(userData);
+		});
+	});
+};
+
